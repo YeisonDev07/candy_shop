9fdbb2396ca694690e3489f33571fa86b4c0b1cb
YeisonDev07 <yeisongil.dev9701@gmail.com>
2025-11-08 20:35:00 -0500

    feat: agregar Prisma y validación a la aplicación

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // elimina propiedades no definidas en el DTO
+      forbidNonWhitelisted: true, // lanza error si vienen propiedades extra
+      transform: true, // transforma los tipos automáticamente (ej. string → number)
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
+  console.log(`🚀 Aplicación corriendo en http://localhost:3000`);
 }
 bootstrap();
